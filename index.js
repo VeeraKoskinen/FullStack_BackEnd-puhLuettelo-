a46bdca589925fc0ca0a9929d2d022644bf6dcc9
Veera Koskinen <veera.koskinen@ren.to>
2018-02-06 17:21:00 +0200

    Tehtävät 3.2 ja 3.3

--- index.js
+++ index.js
@@ -26,8 +26,32 @@ let persons = [
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+}) 
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => {
+        console.log(person.id, typeof person.id, id, typeof id, person.id === id)
+        return person.id === id
+    })
+    
+    console.log(person)
+    if (person) {
+      console.log(person)
+      res.json(person)
+    } else {
+      console.log("Mennään elseen")  
+      res.status(404).end()
+    }
 })
- 
+
+app.get('/info', (req, res) => { 
+    res.send(`
+        <p> puhelinluettelossa ${persons.length}:n henkilön tiedot </p>
+        <p> ${new Date().toUTCString()} </p>
+        `)
+})
+
 
 const port = 3001
 app.listen(port)
