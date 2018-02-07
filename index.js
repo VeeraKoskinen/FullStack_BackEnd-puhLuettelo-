17aae783ae6c318d1c30190c4d400b7922bc6e03
Veera Koskinen <veera.koskinen@ren.to>
2018-02-07 13:31:50 +0200

    Tehtävä 3.6

--- index.js
+++ index.js
@@ -71,6 +71,10 @@ app.post('/api/persons', (req, res) => {
         return res.status(400).json({error: 'content missing'})
     }
 
+    if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({error: 'name must be unique'})
+    }
+
     const person = {
         name: body.name,
         number: body.number,
