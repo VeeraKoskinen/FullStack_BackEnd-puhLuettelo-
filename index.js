6b8b7a759339fab7760bca6f0f0c91f63edcc5f7
Veera Koskinen <veera.koskinen@ren.to>
2018-02-08 12:13:33 +0200

    pieniä korjauksia

--- index.js
+++ index.js
@@ -93,8 +93,13 @@ app.post('/api/persons', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+
+    if (!persons.find(person => person.id ===id)) {
+        res.status(400).end()
+    } else {
+        persons = persons.filter(person => person.id !== id)
+        res.status(204).end()
+    }    
 })
 
 
